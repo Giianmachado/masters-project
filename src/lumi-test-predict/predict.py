33a3c3e338788448d9e2eb9fbdfcb2197b5930f3
Gianluca Maziero Machado <giianmachado@live.com>
2018-09-23 20:30:45 -0300

    (Task, Layout) Format output of predict.py

--- src/lumi-test-predict/predict.py
+++ src/lumi-test-predict/predict.py
@@ -1,4 +1,5 @@
 import os
+import json
 from PIL import Image
 
 from luminoth.tools.checkpoint import get_checkpoint_config
@@ -11,4 +12,4 @@ image = Image.open(os.path.abspath('image.jpg')).convert('RGB')
 objects = network.predict_image(image)
 
 # log objects
-print(objects)
+print json.dumps(objects, indent=4, sort_keys=True)
